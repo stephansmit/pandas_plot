f67693455cd944d9f7ebf7282a8edc4c90e21d05
stephan <stephansmit@hotmail.com>
2019-10-01 11:18:33 +0200

    added the head

--- plot.py
+++ plot.py
@@ -5,6 +5,98 @@ import matplotlib.lines as mlines
 from matplotlib.ticker import FormatStrFormatter
 import os
 
+def plot2Yaxis(df,      groupvariable, groupname, groupunits, groupformat,
+                        xvariable, xname, xunits, xformat,
+                        yvariable, yname, yunits, yformat,
+                        y2variable, y2name, y2units, y2format, 
+                        figpath, figname,fontsize=12,fontsizetitle=14, color=True):
+
+    fig, ax = plt.subplots(figsize=(12,6))
+    ax1 = ax.twinx();  
+    lstyles = ['-','--']
+    labels = [yname, y2name]
+    mstyles = ['o','v','+','*']
+    if color:
+       colors = ['red', 'blue']
+    else:
+       colors = ['k', 'k']
+    grouped = df.sort_values([groupvariable,xvariable],ascending=True) \
+        .groupby(groupvariable)
+
+    # y
+    i=0
+    for name,  group in grouped:
+        grouped.get_group(name).plot(x=xvariable,
+              y=yvariable,
+              ax= ax,
+              color=colors[0],
+              linestyle=lstyles[0],
+              marker=mstyles[i])
+        i+=1
+    ax.set_xlabel(" ".join([xname,'['+xunits+']']), fontsize=fontsize)
+    ax.set_ylabel(" ".join([yname,'['+yunits+']']), fontsize=fontsize)
+    ax.yaxis.label.set_color(colors[0])
+    ax.tick_params(axis='y', colors=colors[0])
+    ax.spines['left'].set_color(colors[0])
+    
+    #y2
+    i=0
+    for name,  group in grouped:
+        grouped.get_group(name).plot(x=xvariable,
+              y=y2variable,
+              ax= ax1,
+              color=colors[1],
+              linestyle=lstyles[1],
+              marker=mstyles[i])
+        i+=1
+
+    ax1.set_xlabel(" ".join([xname,'['+xunits+']']), fontsize=fontsize)
+    ax1.set_ylabel(" ".join([y2name,'['+y2units+']']), fontsize=fontsize)
+    ax1.yaxis.label.set_color(colors[1])
+    ax1.tick_params(axis='y', colors=colors[1])
+    ax1.spines['right'].set_color(colors[1]) 
+
+    #legend     
+    labels = map(lambda x: " ".join([groupname,'=',
+                                      groupformat.format(x),
+                                      groupunits]) if isinstance(x, (np.floating, np.int)) else
+			   " ".join([groupname,'=',x,
+                                     groupunits]),
+                  df.sort_values(groupvariable,ascending=True)[groupvariable] 
+                  .unique())
+    handles = [mlines.Line2D([], 
+                         [], 
+                         color='k',  
+                         linestyle='None',
+                         marker=mstyles[index],
+                         markersize=10, 
+                         label=label) 
+           for index, label in enumerate(labels)]
+              
+    ax.legend(handles=handles, fontsize=fontsize);
+
+    #legend    
+    labels = [yname, y2name]
+    handles = [mlines.Line2D([],
+                         [], 
+                         color=c,  
+                         linestyle=lstyles[index],
+                         markersize=10, 
+                         label=labels[index]) 
+           for index, c in enumerate(colors)]
+              
+    ax1.legend(handles=handles, fontsize=fontsize, loc='upper left')
+    ax.xaxis.set_major_formatter(FormatStrFormatter(xformat))
+    ax.yaxis.set_major_formatter(FormatStrFormatter(yformat))
+    ax1.yaxis.set_major_formatter(FormatStrFormatter(y2format))
+    ax.tick_params(labelsize=fontsize);ax1.tick_params(labelsize=fontsize);
+    
+    fig_file = os.path.join(figpath, figname+".svg")
+    plt.tight_layout();plt.savefig(fig_file)
+    plt.close(fig)
+    fconv = FormatConverter()
+    fconv.convert_to_wmf(figname+".svg", figname+'.wmf', figpath)
+    
 def plot3Yaxis(df,      groupvariable, groupname, groupunits, groupformat,
                         xvariable, xname, xunits, xformat,
                         yvariable, yname, yunits, yformat,
--- test.py
+++ test.py
@@ -2,14 +2,36 @@ import pandas as pd
 from plot import *
 import os
 df = pd.read_csv("test.csv")
-print(df.columns)
-plot3Yaxis(df,      
-              "NumberBlades", "Blades", "#", "{0:.2f}",
+
+plot3Yaxis(df,  "NumberBlades", "Blades", "#", "{0:.2f}",
+               "RotationSpeedHz", "Rotational Speed", "Hz", "%0.2f",
+               "Interface_alpha", "Abs.Flow Angle at Interface", "Deg", "%0.2f",
+               "Interface_beta", "Rel. Flow Angle at Interface", "Deg", "%0.2f", 
+               "Interface_Static_P", "Static Pressure at Outlet", "Pa",  "%0.2f",
+               os.getcwd(), 'angles_interface', color=True)
+plot3Yaxis(df,  "NumberBlades", "Blades", "#", "{0:.2f}",
                "RotationSpeedHz", "Rotational Speed", "Hz", "%0.2f",
                "Outflow_beta", "Rel.Flow Angle at Outlet", "Deg.", "%0.2f",
                "Outflow_alpha", "Abs. Flow Angle at Outlet", "Deg.", "%0.2f", 
                "Interface_beta", "Rel. Flow Angle at Interface", "Deg.",  "%0.2f",
-               os.getcwd(), 'test', color=True)
+               os.getcwd(), 'angles', color=True)
 
+plot3Yaxis(df, "NumberBlades", "Blades", "#", "{0:.2f}",
+               "RotationSpeedHz", "Rotational Speed", "Hz", "%0.2f",
+               "Outflow_Static_P", "Static Pressure Outlet", "Pa", "%0.2f",
+               "Interface_Static_P", "Static Pressure at Interface", "Pa", "%0.2f", 
+               "DegreeOfReaction", "Degree of Reaction", '-',  "%0.2f",
+               os.getcwd(), 'pressures', color=True)
 
-#print(df)
+plot3Yaxis(df, "NumberBlades", "Blades", "#", "{0:.2f}",
+               "RotationSpeedHz", "Rotational Speed", "Hz", "%0.2f",
+               "TotalToStatic", "Total-to-Static Efficiency", "%", "%0.2f",
+               "TotalToTotal", "Total-to-Total Efficiency", "%", "%0.2f", 
+               "Power", "Specific Power", 'kJ/kg',  "%0.2f",
+               os.getcwd(), 'power', color=True)
+
+plot2Yaxis(df, "NumberBlades", "Blades", "#", "{0:.2f}",
+               "RotationSpeedHz", "Rotational Speed", "Hz", "%0.2f",
+               "Outflow_Absolute_thetavel", "Outflow Abs. Tang. Velocity:","m/s", "%0.2f",
+               "Outflow_Relative_thetavel", "Outflow Rel. Tang. Velocity", "m/s", "%0.2f",
+               os.getcwd(), 'velocity', color=True)
